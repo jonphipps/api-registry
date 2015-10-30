ed1b19fd60c8ea12fa7275ac06621b8357bf8bf5
Jon Phipps <jphipps@madcreek.com>
2015-10-30 10:52:09 -0400

    update create_user_table to contain Laravel defaults

--- database/migrations/2015_10_26_164813_create_users_table.php
+++ database/migrations/2015_10_26_164813_create_users_table.php
@@ -16,23 +16,24 @@ class CreateUsersTable extends Migration
 		Schema::create('reg_user', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->integer('id', true);
-			$table->dateTime('created_at');
 			$table->dateTime('last_updated');
 			$table->dateTime('deleted_at');
 			$table->string('nickname', 255);
 			$table->string('salutation', 255);
 			$table->string('first_name', 255);
 			$table->string('last_name', 255);
-			$table->string('email', 255);
+			$table->string('email', 255)->unique();
 			$table->string('sha1_password', 255);
 			$table->string('salt', 255);
 			$table->boolean('want_to_be_moderator');
 			$table->boolean('is_moderator');
 			$table->boolean('is_administrator');
 			$table->integer('deletions');
-			$table->string('password', 255);
+            $table->string('name');
+            $table->string('email')->unique();
+			$table->string('password', 60);
 			$table->string('culture', 255);
+            $table->rememberToken();
 			$table->timestamps();
 			$table->softDeletes();
 		});
