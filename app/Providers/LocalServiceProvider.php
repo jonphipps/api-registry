e6ee1da3aa4d2a95d98080c941315885e8f3c6d8
Jon Phipps <jphipps@madcreek.com>
2015-11-01 13:28:52 -0500

    add localserviceprovider registry

---
+++ app/Providers/LocalServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+    use Barryvdh\Debugbar\Facade;
+    use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+    use Bestmomo\Scafold\ScafoldServiceProvider;
+    use Illuminate\Support\ServiceProvider;
+    use Mitul\Generator\GeneratorServiceProvider;
+    use Way\Generators\GeneratorsServiceProvider;
+    use Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider;
+
+    class LocalServiceProvider extends ServiceProvider
+{
+
+    protected $providers = [
+        GeneratorsServiceProvider::class,
+        MigrationsGeneratorServiceProvider::class,
+        \Barryvdh\Debugbar\ServiceProvider::class,
+        IdeHelperServiceProvider::class,
+        GeneratorServiceProvider::class,
+        ScafoldServiceProvider::class,
+    ];
+    protected $aliases = [
+        'Debugbar'  => Facade::class,
+    ];
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //register the service providers for local environment
+        if ('local' == $this->app->environment() && !empty($this->providers)) {
+            foreach ($this->providers as $provider) {
+                $this->app->register($provider);
+            }
+            //register the alias
+            if (!empty($this->aliases)) {
+                foreach ($this->aliases as $alias => $facade) {
+                    $this->app->alias($alias, $facade);
+                }
+            }
+        }
+
+    }
+}
